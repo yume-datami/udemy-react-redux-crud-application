104a2ec3a3fc3a273b6eca51558ca4b9d17423c7
vagrant <vagrant@localhost.localdomain>
2018-08-07 08:20:27 +0000

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,29 @@
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+// action creator
+export const increment = () =>({
+  type: INCREMENT
+})
+
+// action creator
+export const decrement = () =>({
+  type: DECREMENT
+})
+
+
+
+//// action creator
+//export const increment = () =>{
+//  return {
+//    type: 'INCREMENT'
+//  }
+//}
+//
+//// action creator
+//export const decrement = () =>{
+//  return {
+//    type: 'DECREMENT'
+//  }
+//}
+
