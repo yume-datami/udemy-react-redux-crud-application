0a0462252ab5205851b0767d6323f68b2d752b74
vagrant <vagrant@localhost.localdomain>
2018-08-07 07:37:35 +0000

    learn state and setState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-//import React, { Component } from 'react';
+import React, { Component } from 'react';
+
 //class App extends Component {
 //  render() {
 //    const greeting ="Hi, Tom!";
@@ -18,40 +19,70 @@
 //  }
 //}
 
-import React from 'react';
-import PropTypes from 'prop-types';
-
-const App = () => {
-  const profiles =[
-     { name: "Taro", age: 20 },
-     { name: "Hanako", age: 18 },
-     { name: "NoName", age: 15 }
-  ]
-
-  return (
-    <div>
-      {
-        profiles.map((profile, index) =>{
-          return <User name={profile.name} age={profile.age} key={index}/>
-        })
-      }
-    </div>
-  )}
+//import PropTypes from 'prop-types';
 
 
-const User = (props) => {
-  return <div>Hi, I am {props.name}, and {props.age} years old!</div>
+const App = () => (<Counter></Counter>)
+
+class Counter extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
+  }
+
+   handlePlusButton = () => {
+    this.setState({count: this.state.count + 1})
+  }
+
+  handleMinusButton = () => {
+    this.setState({count: this.state.count - 1})
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
+
+
+
+//const App = () => {
+//  const profiles =[
+//     { name: "Taro", age: 20 },
+//     { name: "Hanako", age: 18 },
+//     { name: "NoName", age: 15 }
+//  ]
+//
+//  return (
+//    <div>
+//      {
+//        profiles.map((profile, index) =>{
+//          return <User name={profile.name} age={profile.age} key={index}/>
+//        })
+//      }
+//    </div>
+//  )}
+//
+//
+//const User = (props) => {
+//  return <div>Hi, I am {props.name}, and {props.age} years old!</div>
+//}
+
 
 //User.defaultProps = {
 //  age: 1
 //}
 
 
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
-}
-
+//User.propTypes = {
+//  name: PropTypes.string,
+//  age: PropTypes.number.isRequired
+//}
 
 export default App;
