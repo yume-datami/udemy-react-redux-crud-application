2a92f1463a9095e95493b82370958833166b6825
vagrant <vagrant@localhost.localdomain>
2018-08-16 02:37:31 +0000

    implement componet with connect function

--- src/components/App.js
+++ src/components/App.js
@@ -1,88 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
-//class App extends Component {
-//  render() {
-//    const greeting ="Hi, Tom!";
-//    const dom = <h1 className="foo">{greeting}</h1>;
-//    return (dom);
-//  }
-//}
+import { increment, decrement } from '../actions'
 
-//class App extends Component {
-//  render() {
-//    return (
-//      <React.Fragment>
-//        <label htmlFor="bar">bar</label>
-//        <input type="text" onChange={() => {console.log("I am clicked!")}} />
-//      </React.Fragment>
-//    )
-//  }
-//}
-
-//import PropTypes from 'prop-types';
-
-
-const App = () => (<Counter></Counter>)
-
-class Counter extends Component {
-
-  constructor(props) {
-    super(props)
-    this.state = { count: 0 }
-  }
-
-   handlePlusButton = () => {
-    this.setState({count: this.state.count + 1})
-  }
-
-  handleMinusButton = () => {
-    this.setState({count: this.state.count - 1})
-  }
+class App extends Component {
 
   render(){
+    const props = this.props
     return (
       <React.Fragment>
-        <div>count: {this.state.count}</div>
-        <button onClick={this.handlePlusButton}>+1</button>
-        <button onClick={this.handleMinusButton}>-1</button>
+        <div>value: {props.value}</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
       </React.Fragment>
     )
   }
 }
 
+const mapStateToProps = state => ({value: state.count.value})
+//const mapDispatchToProps = dispatch => ({
+//  increment: () => dispatch(increment()),
+//  decrement: () => dispatch(decrement()),
+//})
+const mapDispatchToProps = ({increment, decrement})
 
+export default connect(mapStateToProps, mapDispatchToProps)(App)
 
-//const App = () => {
-//  const profiles =[
-//     { name: "Taro", age: 20 },
-//     { name: "Hanako", age: 18 },
-//     { name: "NoName", age: 15 }
-//  ]
-//
-//  return (
-//    <div>
-//      {
-//        profiles.map((profile, index) =>{
-//          return <User name={profile.name} age={profile.age} key={index}/>
-//        })
-//      }
-//    </div>
-//  )}
-//
-//
-//const User = (props) => {
-//  return <div>Hi, I am {props.name}, and {props.age} years old!</div>
-//}
-
-
-//User.defaultProps = {
-//  age: 1
-//}
-
-
-//User.propTypes = {
-//  name: PropTypes.string,
-//  age: PropTypes.number.isRequired
-//}
-
-export default App;
